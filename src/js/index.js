f91d27ad5e04a9269acf734cd9f97d46568fc8ba
stercorexze <stercorexze@gmail.com>
2025-04-19 00:18:14 +0200

    Now all what i wont its scripts for mobile menu.

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,12 @@ tagline();
 
 import timer from './modules/timer.js';
 timer();
+
+import mobileNav from './modules/mobile-nav.js';
+mobileNav();
+//
+// import toggleMobileMenu from './modules/mobile-menu-dropdown.js';
+// toggleMobileMenu();
+
+import toggleMobileMenu from './modules/mobile-menu-dropdown.js';
+toggleMobileMenu();
